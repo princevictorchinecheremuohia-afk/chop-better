46a6167564873c32705ecfa18ede00f0330a1f50
Prince Victor <princevictorchinecheremuohia@gmail.com>
2026-05-18 14:30:30 +0100

    Updated the Categories Page to work on Mobile

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -72,7 +72,7 @@ export default function Navbar() {
         </Link>
 
         <Link
-          href="#"
+          href="/categories"
           onClick={() => setOpen(false)}
           className="text-gray-700 hover:text-orange-500"
         >
